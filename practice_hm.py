9a3f0cdb1072c2da4ff51dcf6386d9a10ffa1013
amayahbaig786 <amayahbaig30@gmail.com>
2025-07-21 17:21:04 +0100

    Add files via upload

---
+++ practice_hm.py
@@ -0,0 +1,19 @@
+import cv2
+
+video = cv2.VideoCapture('video.avi')
+
+car_cascade = cv2.CascadeClassifier('cars.xml')
+
+while True:
+    ret,frames = video.read()
+    gray = cv2.cvtColor(frames,cv2.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(gray,1.1,1)
+    for(x,y,w,h)in cars:
+        cv2.rectangle(frames,(x,y),(x+w,y+h),(100,255,50),2)
+    
+    cv2.imshow("video2",frames)
+
+    if cv2.waitKey(10) == 27:
+        break
+
+cv2.destroyAllWindows()
